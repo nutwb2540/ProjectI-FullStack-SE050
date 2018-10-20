71f0032d4e90320eb15e309928f8959703d38895
nutwb2540 <nutautomic@gmail.com>
2018-10-20 21:02:45 +0700

    fff

--- server.js
+++ server.js
@@ -126,16 +126,7 @@ app.get('/product_delete/:pid', function (req, res) {
         })
 });
 
-<script>
-$(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#myTable tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
-</script>
+
 
 
 var port = process.env.PORT || 8080;
